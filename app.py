229a2313d536b7a780859eef8dac16f14fa670f0
asad-mahmood <60412200+asad-mahmood@users.noreply.github.com>
2021-02-14 01:33:13 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import plotly.express as px
-import numpy as np
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
 
 
 # Web App Title
